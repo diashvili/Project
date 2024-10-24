11033005c294de753451dc2682875f16f097152c
diashvili <114678389+diashvili@users.noreply.github.com>
2024-10-25 00:26:46 +0400

    added repositories

---
+++ Project/Repositories/IPostRepository.cs
@@ -0,0 +1,12 @@
+﻿using Project.Models;
+
+namespace Project.Repositories;
+public interface IPostRepository
+{
+    Task<IEnumerable<Post>> GetAllPostsAsync();
+    Task<Post> GetPostByIdAsync(int id);
+    Task<IEnumerable<Post>> GetPostsByUserIdAsync(int userId);
+    Task AddPostAsync(Post post);
+    Task UpdatePostAsync(Post post);
+    Task DeletePostAsync(int id);
+}
---
+++ Project/Repositories/PostRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Models;
+
+namespace Project.Repositories;
+public class PostRepository : IPostRepository
+{
+    private readonly AppDbContext _context;
+
+    public PostRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Post>> GetAllPostsAsync()
+    {
+        return await _context.Posts.ToListAsync();
+    }
+
+    public async Task<Post> GetPostByIdAsync(int id)
+    {
+        return await _context.Posts.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<Post>> GetPostsByUserIdAsync(int userId)
+    {
+        return await _context.Posts.Where(p => p.UserId == userId).ToListAsync();
+    }
+
+    public async Task AddPostAsync(Post post)
+    {
+        await _context.Posts.AddAsync(post);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdatePostAsync(Post post)
+    {
+        _context.Posts.Update(post);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeletePostAsync(int id)
+    {
+        var post = await GetPostByIdAsync(id);
+        if (post != null)
+        {
+            _context.Posts.Remove(post);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
